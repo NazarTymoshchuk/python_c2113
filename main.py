0f62ba6f5f6822912bd4d162ee81b4811a286c19
NazarTymoshchuk <nazar.tymo@gmail.com>
2024-12-07 13:54:42 +0200

    add methods

--- main.py
+++ main.py
@@ -6,9 +6,33 @@ class Student:
 
     def show_info(self):
        print(f"Name: {self.name}, progress: {self.progress}, energy: {self.energy}")
+
+    def sleep(self):
+       print("Sleeping")
+       self.energy += 15
+
+    def study(self):
+       print("Studing")
+       self.progress += 0.5
+       self.energy -= 5
+
+    def chill(self):
+       print("chilling")
+       self.energy += 10
+       self.progress -= 0.2
+
+    
+
+
       
 
 
 student = Student("Nikita")
+
+student.show_info()
+
+student.study()
+student.chill()
+student.sleep()
 
 student.show_info()
